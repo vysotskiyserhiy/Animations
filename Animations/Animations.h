8ed72e129b516b3f28468c3c8f5c6a45fc0d4807
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2019-03-09 11:51:24 +0200

    fixes rename

---
+++ Animations/Animations.h
@@ -0,0 +1,19 @@
+//
+//  Animations.h
+//  Animations
+//
+//  Created by Serhiy Vysotskiy on 3/1/19.
+//  Copyright © 2019 Serhiy Vysotskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animator.
+FOUNDATION_EXPORT double AnimationsVersionNumber;
+
+//! Project version string for Animator.
+FOUNDATION_EXPORT const unsigned char AnimationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animations/PublicHeader.h>
+
+
